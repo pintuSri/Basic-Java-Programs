cf2751fa929f0cbe613364193d21f74f9b472b9d
Sridhar <interiaengg@gmail.com>
2024-11-21 18:47:47 +0530

    AddTwoNumbers Java Program

---
+++ AddTwoNumbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class AddTwoNumbers{
+	
+	public void add(int x, int y){
+		System.out.println(x+y);
+	}
+	
+	public static void main(String[] args){
+		AddTwoNumbers obj = new AddTwoNumbers();
+		obj.add(10,12);
+	}
+}
